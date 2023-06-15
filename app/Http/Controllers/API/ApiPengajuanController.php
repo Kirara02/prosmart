730a1afaef7ea6e1c3b2081eaa81dc1c4c5b1058
Kirara02 <fathul.sirnaraja92@gmail.com>
2023-06-15 16:00:19 +0700

    add try catch

--- app/Http/Controllers/API/ApiPengajuanController.php
+++ app/Http/Controllers/API/ApiPengajuanController.php
@@ -14,36 +14,44 @@ class ApiPengajuanController extends Controller
 {
     public function post(Request $request)
     {
-        $ktp = $request->file('ktp');
-        $imgKtp = $ktp->storeAs('images/ktp', Str::slug($request->nama_pemohon) . '-' . Str::random(6) . '.' . $ktp->extension());
+        try {
 
-        $data = Pengajuan::create([
-            'nama_pemohon' => $request->nama_pemohon,
-            'nama_terdakwa' => $request->nama_terdakwa,
-            'no_handphone' => $request->no_handphone,
-            'alamat' => $request->alamat,
-            'ktp' => $imgKtp,
-            'catatan' => $request->catatan
-        ]);
+            $ktp = $request->file('ktp');
+            $imgKtp = $ktp->storeAs('images/ktp', Str::slug($request->nama_pemohon) . '-' . Str::random(6) . '.' . $ktp->extension());
+
+            $data = Pengajuan::create([
+                'nama_pemohon' => $request->nama_pemohon,
+                'nama_terdakwa' => $request->nama_terdakwa,
+                'no_handphone' => $request->no_handphone,
+                'alamat' => $request->alamat,
+                'ktp' => $imgKtp,
+                'catatan' => $request->catatan
+            ]);
 
 
-        $bukti = $request->file('bukti');
-        $insert = [];
-        foreach ($bukti as $key=>$image) {
-            $imgBukti = $image->storeAs('images/bukti-kepemilikan', Str::slug($request->nama_pemohon) . '-' . Str::random(6) . '.' . $image->extension());
-            $insert[$key]['id_pengajuan'] = $data->id;
-            $insert[$key]['image'] = $imgBukti;
-            $insert[$key]['created_at'] = Carbon::now();
-            $insert[$key]['updated_at'] = Carbon::now();
+            $bukti = $request->file('bukti');
+            $insert = [];
+            foreach ($bukti as $key=>$image) {
+                $imgBukti = $image->storeAs('images/bukti-kepemilikan', Str::slug($request->nama_pemohon) . '-' . Str::random(6) . '.' . $image->extension());
+                $insert[$key]['id_pengajuan'] = $data->id;
+                $insert[$key]['image'] = $imgBukti;
+                $insert[$key]['created_at'] = Carbon::now();
+                $insert[$key]['updated_at'] = Carbon::now();
+            }
+
+            BuktiGallery::insert($insert);
+
+            $result = Pengajuan::with(['gallery'])->find($data->id);
+
+            return ResponseFormatter::success(
+                $result,
+                'Data berhasil diupload'
+            );
+        } catch (\Throwable $th) {
+            return ResponseFormatter::error(
+                null,
+                'Data gagal di upload'
+            );
         }
-
-        BuktiGallery::insert($insert);
-
-        $result = Pengajuan::with(['gallery'])->find($data->id);
-
-        return ResponseFormatter::success(
-            $result,
-            'Data berhasil diupload'
-        );
     }
 }
