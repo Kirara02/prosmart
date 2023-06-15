596721ecbc7247d19d6f633b1289a79938ea97f1
Kirara02 <fathul.sirnaraja92@gmail.com>
2023-06-15 15:52:49 +0700

    add post pengajuan api

--- app/Http/Controllers/API/ApiPengajuanController.php
+++ app/Http/Controllers/API/ApiPengajuanController.php
@@ -2,28 +2,19 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\BuktiGallery;
 use App\Models\Pengajuan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class ApiPengajuanController extends Controller
 {
-    public function post(Request $request){
-        $request->validate([
-            'tgl_pengajuan' => 'required',
-            'nama_pemohon' => 'required|string',
-            'nama_terdakwa' => 'string',
-            'no_handphone' => 'required|string',
-            'jenis_barang_bukti' => 'required|string',
-            'alamat' => 'required|string',
-            'ktp' => 'required|image|mimes:jpeg,jpg,png|max:512',
-            'bukti.*' => 'required|image|mimes:jpeg,jpg,png|max:1024',
-            'catatan' => 'string'
-        ]);
-
-        $ktp = $request->ktp;
-        $imgKtp = $ktp->storeAs('images/ktp', Str::slug($request->nama_pemohon) . '-' . Str::random(6). '.' . $ktp->extension());
+    public function post(Request $request)
+    {
+        $ktp = $request->file('ktp');
+        $imgKtp = $ktp->storeAs('images/ktp', Str::slug($request->nama_pemohon) . '-' . Str::random(6) . '.' . $ktp->extension());
 
         $data = Pengajuan::create([
             'nama_pemohon' => $request->nama_pemohon,
@@ -34,5 +25,22 @@ class ApiPengajuanController extends Controller
             'catatan' => $request->catatan
         ]);
 
+
+        $bukti = $request->file('bukti');
+        $insert = [];
+        foreach ($bukti as $key=>$image) {
+            $imgBukti = $image->storeAs('images/bukti-kepemilikan', Str::slug($request->nama_pemohon) . '-' . Str::random(6) . '.' . $image->extension());
+            $insert[$key]['id_pengajuan'] = $data->id;
+            $insert[$key]['image'] = $imgBukti;
+        }
+
+        BuktiGallery::insert($insert);
+
+        $result = Pengajuan::with(['gallery'])->find($data->id);
+
+        return ResponseFormatter::success(
+            $result,
+            'Data berhasil diupload'
+        );
     }
 }
